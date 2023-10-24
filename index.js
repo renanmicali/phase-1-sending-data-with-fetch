be677490f7b116f2122e2166b42499d0a6bb2cc4
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-24 14:42:41 +1100

    finalized

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Add your code here
+// const init = () => {
+//     const formData = {
+//         dogName: "Byron",
+//         dogBreed: "Poodle",
+//     };
+
+
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+//     fetch("http://localhost:3000/dogs", configurationObject)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (object) {
+//             console.log(object);
+//         })
+//         .catch(function (error) {
+//             alert("Bad things! Ragnarők!");
+//             console.log(error.message);
+//         });
+
+// }
+
+
+const body = document.querySelector("body");
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json", Accept: "application/json" },
+        body: JSON.stringify({
+            name,
+            email,
+        }),
+    })
+        .then((res) => res.json())
+        .then((data) => (document.body.innerHTML = data["id"]))
+        .catch((err) => {
+            const error = document.createElement("div");
+            error.innerHTML = "Unauthorized Access";
+            body.append(error);
+        });
+}
